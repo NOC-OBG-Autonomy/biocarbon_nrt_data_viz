bdaab025e263181e3f37e0fb738f71585a94dd01
Flavien Petit <flavien.petit@obs-vlfr.fr>
2024-07-23 11:40:30 +0100

    A bash script to upadte uk float position

---
+++ C2_api/get_last_uk_float_pos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Execute the Python script
+echo "Running Python script..."
+"c:/Users/flapet/OneDrive - NOC/Documents/NRT_viz/biocarbon_nrt_data_viz/nrt_env/Scripts/python.exe" sftp_download.py
+
+# Check if the Python script executed successfully
+if [ $? -ne 0 ]; then
+    echo "Python script failed. Exiting."
+    exit 1
+fi
+
+# Execute the MATLAB script
+echo "Running MATLAB script..."
+matlab -nodisplay -nosplash -r "run('C:\Users\flapet\OneDrive - NOC\Documents\NRT_viz\biocarbon_nrt_data_viz\Data\Floats\bin_files\parse_provor_messages.m'); exit;"
+
+# Check if the MATLAB script executed successfully
+if [ $? -ne 0 ]; then
+    echo "MATLAB script failed. Exiting."
+    exit 1
+fi
+
+echo "Both scripts executed successfully. Wait for matlab to process..."

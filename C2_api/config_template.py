190fde2ac51d6978d3444cd0401c9b65187ff0dc
Flavien Petit <flavien.petit@obs-vlfr.fr>
2024-08-20 10:08:48 +0100

    update config yml template

--- C2_api/config_template.py
+++ C2_api/config_template.py
@@ -1,8 +1,10 @@
-token = "yourtoken" 
+token = "your token in the prod env" 
+token_test = "your token in the test env"
+vpn_access = False #Changing this to True will lead to a request to the broker
 
 #broker
 broker = "seasystems.noc.soton.ac.uk"
 port = 1883
 topic = "DY/POSMV/data/GPGGA"
-username = "username"
-password = "pwd"
+username = "DY180_flavien_petit"
+password = "h@ppySong75"
